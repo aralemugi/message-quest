e20e90ecc62d4df5502edaaa4de85d8e95d2e1df
Taro <pepperoni.4.eg@gmail.com>
2023-01-21 14:30:53 +0900

    fix map

--- lib/maps/start_map.dart
+++ lib/maps/start_map.dart
@@ -14,8 +14,8 @@ class StartMap extends StatefulWidget {
 }
 
 class _StartMapState extends State<StartMap> {
-  final tileHeightSize = 40.0;
-  final tileWidthSize = 40.0; // タイルのサイズ定義
+  final tileHeightSize = 50.0;
+  final tileWidthSize = 50.0; // タイルのサイズ定義
 
   @override
   Widget build(BuildContext context) {
